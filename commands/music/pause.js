d338289e849437d3377b2d60a0097f51470d4948
Fyphen <fyphensub@gmail.com>
2024-05-14 03:02:54 +0000

    chore: Update discord.js import in music commands

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -13,7 +13,6 @@ const {
 	ActionRowBuilder,
 	EmbedBuilder,
 } = require('discord.js');
-const listenEvents = require('../../util/playerEvent.js');
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('pause').setDescription('Pause music'),
