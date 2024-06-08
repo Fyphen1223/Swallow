60f7de7152ebbaf2f3ad57f431d1f7669f947843
Fyphen <fyphensub@gmail.com>
2024-06-08 07:27:40 +0000

    chore: Add stop functionality to music commands
    This commit adds the "stop" functionality to the music commands in both the `en.json` and `ja.json` files. It includes the translation for the "stop" action and updates the language files accordingly.
    
    Note: The commit message has been generated based on the provided code changes and recent repository commits.

---
+++ commands/music/stop.js
@@ -0,0 +1,47 @@
+const { getLocale } = require('../../lang/lang.js');
+const { createMessageEmbed } = require('../../util/embed.js');
+
+const guilds = require('../../data/guilds.json');
+
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder().setName('stop').setDescription('Stop playback'),
+	info: {
+		premium: false,
+	},
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		const guildId = interaction.guild.id;
+		if (!interaction.member.voice.channelId || !globalThis.queue[guildId]) {
+			const noValidVCEmbed = createMessageEmbed(
+				getLocale(guilds[guildId].locale).vc.noVC,
+				interaction
+			);
+			await interaction.editReply({ embeds: [noValidVCEmbed] });
+			return;
+		}
+
+		if (globalThis.queue[guildId].voiceChannel) {
+			if (
+				globalThis.queue[guildId].voiceChannel.id !==
+				interaction.member.voice.channelId
+			) {
+				const differentVCEmbed = createMessageEmbed(
+					getLocale(guilds[guildId].locale).vc.differentVC,
+					interaction
+				);
+				await interaction.editReply({ embeds: [differentVCEmbed] });
+				return;
+			}
+		}
+
+		await globalThis.queue[guildId].player.stop();
+		await globalThis.queue[guildId].player.node.leaveVoiceChannel(guildId);
+
+		const embed = createMessageEmbed(getLocale(guilds[guildId].locale).vc.stop);
+
+		await interaction.editReply({ embeds: [embed] });
+	},
+};
