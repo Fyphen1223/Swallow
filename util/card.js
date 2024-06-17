2846cc0eeada4f972efcc39cea21b525e6e2f7a5
Fyphen <fyphensub@gmail.com>
2024-06-17 01:43:48 +0000

    chore: Refactor generateMusicCard function and remove unused code

--- util/card.js
+++ util/card.js
@@ -168,16 +168,4 @@ function formatSource(string) {
 	}
 }
 
-function rgbToHex(rgb) {
-	return (
-		'#' +
-		rgb
-			.map((x) => {
-				const hex = x.toString(16);
-				return hex.length === 1 ? '0' + hex : hex;
-			})
-			.join('')
-	);
-}
-
 module.exports = { generateMusicCard };
